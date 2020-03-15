ebc1d8ecf5f971670f9f1ee46bc7bf826d4706b6
108aea008 <t108aea008@ntut.org.tw>
2020-03-15 19:31:59 +0800

    小修改

--- MinecraftBedrockRedirect/Form1.Designer.cs
+++ MinecraftBedrockRedirect/Form1.Designer.cs
@@ -64,7 +64,6 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(112, 21);
             this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "000.000.000.000";
             // 
             // label1
             // 
